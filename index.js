63212ad62e2cc8c8e962998cb3ecfa99beae0c30
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-17 04:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name){
   console.log('Happy holidays,${name}!')
   return ('Happy holidays, ${name}!')
    }
- happyHolidaysTo('you')
+ 
    
 function happyHolidaysTo(holiday, name){
   console.log('Happy ${holiday}, ${name}!')
