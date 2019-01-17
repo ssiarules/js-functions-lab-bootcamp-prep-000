72a6cdfdd2c9bb0aef736555f4a059982aeb03df
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-17 04:05:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+function happyHolidays(){
+  console.log("Happy holidays!")
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name){
+  console.log('Happy holidays,${name}!')
+  return ('Happy holidays, ${name}!')
+   }
+ happyHolidaysTo('you')
+   
+function happyHolidaysTo(holiday, name){
+  console.log('Happy ${holiday}, ${name}!')
+  return 'Happy ${holiday}, ${name}!'
+   }
+happyHolidaysTo('Independence Day', 'you')
+
+function holidayCountdown(){
+  console.log('It\'s ${days} days until ${holiday}!')
+  return 'It\'s ${days} days until ${holiday}!'
+}
+holidayCountdown("Mother's Day", 20)
+   
